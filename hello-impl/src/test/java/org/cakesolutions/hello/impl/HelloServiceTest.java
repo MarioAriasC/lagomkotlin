de76f93f07d115e610151fce17d30efea15e4205
Ilya Ryzhenkov <orangy@jetbrains.com>
2017-03-31 14:29:02 +0300

    Remove "!" from test since it is not sent by a service

--- hello-impl/src/test/java/org/cakesolutions/hello/impl/HelloServiceTest.java
+++ hello-impl/src/test/java/org/cakesolutions/hello/impl/HelloServiceTest.java
@@ -20,14 +20,14 @@ public class HelloServiceTest {
       HelloService service = server.client(HelloService.class);
 
       String msg1 = service.hello("Alice").invoke().toCompletableFuture().get(5, SECONDS);
-      assertEquals("Hello, Alice!", msg1); // default greeting
+      assertEquals("Hello, Alice", msg1); // default greeting
 
       service.useGreeting("Alice").invoke(new KGreetingMessage("Hi")).toCompletableFuture().get(5, SECONDS);
       String msg2 = service.hello("Alice").invoke().toCompletableFuture().get(5, SECONDS);
-      assertEquals("Hi, Alice!", msg2);
+      assertEquals("Hi, Alice", msg2);
 
       String msg3 = service.hello("Bob").invoke().toCompletableFuture().get(5, SECONDS);
-      assertEquals("Hello, Bob!", msg3); // default greeting
+      assertEquals("Hello, Bob", msg3); // default greeting
     });
   }
 
